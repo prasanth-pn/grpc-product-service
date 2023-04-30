7097edb3656a0591a4a4648998ce1c147ea7cc5b
PRASANTH PN <118249131+prasanth-pn@users.noreply.github.com>
2023-04-30 21:34:27 +0530

    commited

--- cmd/main.go
+++ cmd/main.go
@@ -5,9 +5,9 @@ import (
 	"log"
 	"net"
 
+	"github.com/prasanth-pn/grpc-product-service/pkg/api/handlers"
 	"github.com/prasanth-pn/grpc-product-service/pkg/config"
 	"github.com/prasanth-pn/grpc-product-service/pkg/db"
-	"github.com/prasanth-pn/grpc-product-service/pkg/api/handlers"
 	"github.com/prasanth-pn/grpc-product-service/pkg/pb"
 	"github.com/prasanth-pn/grpc-product-service/pkg/repository"
 	"github.com/prasanth-pn/grpc-product-service/pkg/usecase"
@@ -33,12 +33,12 @@ func main() {
 	ProductRepository := repository.NewAuthRepository(sqlDB)
 	productUsecase := usecase.NewProductUseCase(ProductRepository)
 	s := handlers.NeWProductHandler(productUsecase)
-	grpcServer:=grpc.NewServer()
+	grpcServer := grpc.NewServer()
 
-	pb.RegisterProductServiceServer(grpcServer,s)
+	pb.RegisterProductServiceServer(grpcServer, s)
 
-	if err:=grpcServer.Serve(lis);err!=nil{
-		log.Fatalf("failed to start the server %s",err)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to start the server %s", err)
 	}
 
 }
