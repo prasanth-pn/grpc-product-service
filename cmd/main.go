23c3a3a676948496fab92612a5e41b3bed765a3a
PRASANTH PN <118249131+prasanth-pn@users.noreply.github.com>
2023-04-30 20:30:11 +0530

    want to go repository function to complete some functions

--- cmd/main.go
+++ cmd/main.go
@@ -3,16 +3,27 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 
 	"github.com/prasanth-pn/grpc-product-service/pkg/config"
+	"github.com/prasanth-pn/grpc-product-service/pkg/db"
 )
 
 func main() {
 	c, err := config.LoadConfig()
-	fmt.Println(c,"port")
-	if err!=nil{
-		log.Fatalln("failed at config",err)
+	fmt.Println(c, "port")
+	if err != nil {
+		log.Fatalln("failed at config", err)
 	}
-	
+	_, err = db.ConnectGorm(c)
+	if err != nil {
+		log.Fatalf("error connecting gorm : %s", err)
+	}
+	lis, err := net.Listen("tcp", c.DBPort)
+	if err!=nil{
+		log.Fatalf("failed to listening:",err)
+	}
+	sqlDB:=db.ConnectDB(c)
+	ProductRepository:=repository.NewAuthRepository
 
 }
