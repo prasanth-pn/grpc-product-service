b2690e5436e11d09220453f43b0ebc1dad5ad3c1
PRASANTH PN <118249131+prasanth-pn@users.noreply.github.com>
2023-04-30 20:52:50 +0530

    main function is ready

--- cmd/main.go
+++ cmd/main.go
@@ -7,8 +7,11 @@ import (
 
 	"github.com/prasanth-pn/grpc-product-service/pkg/config"
 	"github.com/prasanth-pn/grpc-product-service/pkg/db"
+	"github.com/prasanth-pn/grpc-product-service/pkg/api/handlers"
+	"github.com/prasanth-pn/grpc-product-service/pkg/pb"
 	"github.com/prasanth-pn/grpc-product-service/pkg/repository"
 	"github.com/prasanth-pn/grpc-product-service/pkg/usecase"
+	"google.golang.org/grpc"
 )
 
 func main() {
@@ -22,12 +25,20 @@ func main() {
 		log.Fatalf("error connecting gorm : %s", err)
 	}
 	lis, err := net.Listen("tcp", c.DBPort)
-	if err!=nil{
-		log.Fatalf("failed to listening:",err)
+
+	if err != nil {
+		log.Fatalf("failed to listening:", err)
 	}
-	sqlDB:=db.ConnectDB(c)
-	ProductRepository:=repository.NewAuthRepository(sqlDB)
-	productUsecase:=usecase.NewProductUseCase(ProductRepository)
-	handler.
+	sqlDB := db.ConnectDB(c)
+	ProductRepository := repository.NewAuthRepository(sqlDB)
+	productUsecase := usecase.NewProductUseCase(ProductRepository)
+	s := handlers.NeWProductHandler(productUsecase)
+	grpcServer:=grpc.NewServer()
+
+	pb.RegisterProductServiceServer(grpcServer,s)
+
+	if err:=grpcServer.Serve(lis);err!=nil{
+		log.Fatalf("failed to start the server %s",err)
+	}
 
 }
